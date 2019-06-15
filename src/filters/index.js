831c552950308f99d3cecf786d1753d8bc17cd19
406803045 <406803045@qq.com>
2019-06-15 14:41:40 +0800

    filters

--- src/filters/index.js
+++ src/filters/index.js
@@ -3,4 +3,5 @@ import * as filter from './filter'
 
 Object.keys(filter).forEach(k => Vue.filter(k, filter[k]))
 
+Vue.prototype.$formatDate = Vue.filter('formatDate')
 Vue.prototype.$hidePhone = Vue.filter('hidePhone')
