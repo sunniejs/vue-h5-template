a0582f145a395262b669cc76a0e21f44f62f6d3a
宋楠 <406803045@qq.com>
2020-02-07 16:26:24 +0800

    解决build 报错

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,12 +1,12 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 module.exports = {
-  plugins: {
-    autoprefixer: {
-      browsers: ['Android >= 4.0', 'iOS >= 7']
-    },
-    'postcss-pxtorem': {
-      rootValue: 37.5,
-      propList: ['*']
+    plugins: {
+        autoprefixer: {
+            overrideBrowserslist: ['Android 4.1', 'iOS 7.1', 'Chrome > 31', 'ff > 31', 'ie >= 8']
+        },
+        'postcss-pxtorem': {
+            rootValue: 37.5,
+            propList: ['*']
+        }
     }
-  }
 }
