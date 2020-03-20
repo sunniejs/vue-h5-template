18372b1e4ebb698a223c9f557878d78c9a578de8
sunnie <406803045@qq.com>
2020-03-20 17:45:39 +0800

    修复ie报错 本地环境运行

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+//  IE 兼容
+import '@babel/polyfill'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -7,8 +9,7 @@ import '@/assets/css/index.scss'
 
 // 全局引入按需引入UI库 vant
 import '@/plugins/vant'
-//  IE 兼容
-import '@babel/polyfill'
+
 // 移动端适配
 import 'lib-flexible/flexible.js'
 
