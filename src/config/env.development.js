2a1687c0cc82910b01a4fd260ff376d0ab52a8da
sunnie <406803045@qq.com>
2020-04-29 11:07:35 +0800

    代理

--- src/config/env.development.js
+++ src/config/env.development.js
@@ -2,7 +2,7 @@
 module.exports = {
   title: 'vue-h5-template',
   baseUrl: 'http://localhost:9018', // 项目地址
-  baseApi: 'https://test.xxx.com/api', // 本地api请求地址
+  baseApi: 'https://test.xxx.com/api', // 本地api请求地址,注意：如果你使用了代理，请设置成'/'
   APPID: 'xxx',
   APPSECRET: 'xxx',
   $cdn:'https://imgs.solui.cn'
