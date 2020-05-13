4f5d9ef5360b4ad7271d852bd5b67ac6a3d5582d
于博文 <wx_ab2d8b2e35cf49e98fb21616c8457bea@git.code.tencent.com>
2020-05-13 10:26:43 +0800

    @vue/cli 已经修复了打开两次浏览器问题

--- vue.config.js
+++ vue.config.js
@@ -45,7 +45,7 @@ module.exports = {
   productionSourceMap: false, // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
   devServer: {
     port: 9020, // 端口
-    open: false, // 启动后打开浏览器
+    open: true, // 启动后打开浏览器
     overlay: {
       //  当出现编译器错误或警告时，在浏览器中显示全屏覆盖层
       warnings: false,
