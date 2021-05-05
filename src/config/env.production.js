f1ebab4b9c468026d71d3572c06f5543097dbbd7
zhaochuanxin <zhaochuanxin@example.com>
2021-05-05 10:25:06 +0800

    修改为测试地址

--- src/config/env.production.js
+++ src/config/env.production.js
@@ -1,8 +1,8 @@
 // 正式
 module.exports = {
   title: '徒步大会活动',
-  baseUrl: 'https://mall.xiangleba.com.cn', // 正式项目地址
-  baseApi: 'https://mall.xiangleba.com.cn', // 正式api请求地址
+  baseUrl: 'https://vall.xiangleba.com.cn', // 正式项目地址
+  baseApi: 'https://vall.xiangleba.com.cn', // 正式api请求地址
   APPID: 'xxx',
   APPSECRET: 'xxx',
   $cdn: 'https://www.sunniejs.cn/static'
