f4c85edb0b5979464233d8384344548a2fb4d683
fonghehe <fonghehe@users.noreply.github.com>
2022-08-19 10:33:00 +0000

    deploy: d43e685e68445fccc714fe5bdd8a16e2a704d8e9

---
+++ assets/guide_vue2_start.md.4f70c5c5.js
@@ -0,0 +1,14 @@
+import{_ as s,c as a,o as n,a as e}from"./app.3b30a875.js";const A=JSON.parse('{"title":"\u542F\u52A8\u9879\u76EE","description":"","frontmatter":{},"headers":[],"relativePath":"guide/vue2/start.md","lastUpdated":1660905131000}'),l={name:"guide/vue2/start.md"},p=e(`<h1 id="\u542F\u52A8\u9879\u76EE" tabindex="-1">\u542F\u52A8\u9879\u76EE <a class="header-anchor" href="#\u542F\u52A8\u9879\u76EE" aria-hidden="true">#</a></h1><div class="language-js"><button class="copy"></button><span class="lang">js</span><pre><code><span class="line"></span>
+<span class="line"><span style="color:#676E95;">// \u62C9\u53D6\u9879\u76EE</span></span>
+<span class="line"><span style="color:#A6ACCD;">git clone </span><span style="color:#FFCB6B;">https</span><span style="color:#89DDFF;">:</span><span style="color:#676E95;">//github.com/sunniejs/vue-h5-template</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;">// \u5207\u6362\u5206\u652F</span></span>
+<span class="line"><span style="color:#A6ACCD;">git checkout </span><span style="color:#89DDFF;">-</span><span style="color:#A6ACCD;">b vue2</span><span style="color:#89DDFF;">-</span><span style="color:#A6ACCD;">h5</span><span style="color:#89DDFF;">-</span><span style="color:#A6ACCD;">template origin</span><span style="color:#89DDFF;">/</span><span style="color:#A6ACCD;">vue2</span><span style="color:#89DDFF;">-</span><span style="color:#A6ACCD;">h5</span><span style="color:#89DDFF;">-</span><span style="color:#A6ACCD;">template</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;">// \u5B89\u88C5\u4F9D\u8D56</span></span>
+<span class="line"><span style="color:#A6ACCD;">yarn install</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;">// \u542F\u52A8\u9879\u76EE</span></span>
+<span class="line"><span style="color:#A6ACCD;">yarn serve</span></span>
+<span class="line"></span>
+<span class="line"></span></code></pre></div>`,2),t=[p];function o(c,r,i,_,y,D){return n(),a("div",null,t)}const C=s(l,[["render",o]]);export{A as __pageData,C as default};
